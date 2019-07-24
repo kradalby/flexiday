5f8609ce0976ed301b8f6df861498d85f22c8e72
Kristoffer Dalby <kradalby@kradalby.no>
2019-07-24 20:33:44 +0200

    adds 2020 holidays

--- src/Holiday.elm
+++ src/Holiday.elm
@@ -42,6 +42,21 @@ holidays campus =
         EAC ->
             eacHolidays
 
+estecHolidays2020 : List Date.Date
+estecHolidays2020 =
+    [ Date.date 2020 1 1
+    , Date.date 2020 4 13
+    , Date.date 2020 4 27
+    , Date.date 2020 5 5
+    , Date.date 2020 5 21
+    , Date.date 2020 6 1
+    , Date.date 2020 12 24
+    , Date.date 2020 12 25
+    , Date.date 2020 12 28
+    , Date.date 2020 12 29
+    , Date.date 2020 12 30
+    , Date.date 2020 12 31
+    ]
 
 estecHolidays2019 : List Date.Date
 estecHolidays2019 =
@@ -93,8 +108,23 @@ estecHolidays2017 =
 
 estecHolidays : List Date.Date
 estecHolidays =
-    estecHolidays2017 ++ estecHolidays2018 ++ estecHolidays2019
+    estecHolidays2017 ++ estecHolidays2018 ++ estecHolidays2019 ++ estecHolidays2020
 
+hqHolidays2020 : List Date.Date
+hqHolidays2020 =
+    [ Date.date 2020 1 1
+    , Date.date 2020 1 2
+    , Date.date 2020 1 3
+    , Date.date 2020 4 13
+    , Date.date 2020 5 1
+    , Date.date 2020 5 21
+    , Date.date 2020 7 14
+    , Date.date 2020 12 25
+    , Date.date 2020 12 28
+    , Date.date 2020 12 29
+    , Date.date 2020 12 30
+    , Date.date 2020 12 31
+    ]
 
 hqHolidays2019 : List Date.Date
 hqHolidays2019 =
@@ -142,7 +172,23 @@ hqHolidays2017 =
 
 hqHolidays : List Date.Date
 hqHolidays =
-    hqHolidays2017 ++ hqHolidays2018 ++ hqHolidays2019
+    hqHolidays2017 ++ hqHolidays2018 ++ hqHolidays2019 ++ hqHolidays2020
+
+esrinHolidays2020 : List Date.Date
+esrinHolidays2020 =
+    [ Date.date 2020 1 1
+    , Date.date 2020 1 6
+    , Date.date 2020 4 13
+    , Date.date 2020 5 1
+    , Date.date 2020 6 2
+    , Date.date 2020 12 8
+    , Date.date 2020 12 24
+    , Date.date 2020 12 25
+    , Date.date 2020 12 28
+    , Date.date 2020 12 29
+    , Date.date 2020 12 30
+    , Date.date 2020 12 31
+    ]
 
 
 esrinHolidays2019 : List Date.Date
@@ -190,8 +236,23 @@ esrinHolidays2017 =
 
 esrinHolidays : List Date.Date
 esrinHolidays =
-    esrinHolidays2017 ++ esrinHolidays2018 ++ esrinHolidays2019
+    esrinHolidays2017 ++ esrinHolidays2018 ++ esrinHolidays2019 ++ esrinHolidays2020
 
+esocHolidays2020 : List Date.Date
+esocHolidays2020 =
+    [ Date.date 2020 1 1
+    , Date.date 2020 2 24
+    , Date.date 2020 4 10
+    , Date.date 2020 4 13
+    , Date.date 2020 5 1
+    , Date.date 2020 5 21
+    , Date.date 2020 5 22
+    , Date.date 2020 6 1
+    , Date.date 2020 6 11
+    , Date.date 2020 6 12
+    , Date.date 2020 12 25
+    , Date.date 2020 12 31
+    ]
 
 esocHolidays2019 : List Date.Date
 esocHolidays2019 =
@@ -235,8 +296,23 @@ esocHolidays2017 =
 
 esocHolidays : List Date.Date
 esocHolidays =
-    esocHolidays2017 ++ esocHolidays2018 ++ esocHolidays2019
+    esocHolidays2017 ++ esocHolidays2018 ++ esocHolidays2019 ++ esocHolidays2020
 
+esacHolidays2020 : List Date.Date
+esacHolidays2020 =
+    [ Date.date 2020 1 1
+    , Date.date 2020 1 6
+    , Date.date 2020 4 9
+    , Date.date 2020 4 10
+    , Date.date 2020 5 1
+    , Date.date 2020 5 15
+    , Date.date 2020 10 12
+    , Date.date 2020 11 2
+    , Date.date 2020 12 7
+    , Date.date 2020 12 8
+    , Date.date 2020 12 24
+    , Date.date 2020 12 25
+    ]
 
 esacHolidays2019 : List Date.Date
 esacHolidays2019 =
@@ -282,8 +358,23 @@ esacHolidays2017 =
 
 esacHolidays : List Date.Date
 esacHolidays =
-    esacHolidays2017 ++ esacHolidays2018 ++ esacHolidays2019
+    esacHolidays2017 ++ esacHolidays2018 ++ esacHolidays2019 ++ esacHolidays2020
 
+ecsatHolidays2020 : List Date.Date
+ecsatHolidays2020 =
+    [ Date.date 2020 1 1
+    , Date.date 2020 4 10
+    , Date.date 2020 4 13
+    , Date.date 2020 5 8
+    , Date.date 2020 5 25
+    , Date.date 2020 8 31
+    , Date.date 2020 12 24
+    , Date.date 2020 12 25
+    , Date.date 2020 12 28
+    , Date.date 2020 12 29
+    , Date.date 2020 12 30
+    , Date.date 2020 12 31
+    ]
 
 ecsatHolidays2019 : List Date.Date
 ecsatHolidays2019 =
@@ -331,8 +422,23 @@ ecsatHolidays2017 =
 
 ecsatHolidays : List Date.Date
 ecsatHolidays =
-    ecsatHolidays2017 ++ ecsatHolidays2018 ++ ecsatHolidays2019
+    ecsatHolidays2017 ++ ecsatHolidays2018 ++ ecsatHolidays2019 ++ ecsatHolidays2020
 
+eacHolidays2020 : List Date.Date
+eacHolidays2020 =
+    [ Date.date 2020 1 1
+    , Date.date 2020 4 10
+    , Date.date 2020 4 13
+    , Date.date 2020 5 1
+    , Date.date 2020 5 21
+    , Date.date 2020 5 22
+    , Date.date 2020 6 1
+    , Date.date 2020 6 11
+    , Date.date 2020 6 12
+    , Date.date 2020 12 24
+    , Date.date 2020 12 25
+    , Date.date 2020 12 31
+    ]
 
 eacHolidays2019 : List Date.Date
 eacHolidays2019 =
@@ -377,4 +483,4 @@ eacHolidays2017 =
 
 eacHolidays : List Date.Date
 eacHolidays =
-    eacHolidays2017 ++ eacHolidays2018 ++ eacHolidays2019
+    eacHolidays2017 ++ eacHolidays2018 ++ eacHolidays2019 ++ eacHolidays2020
